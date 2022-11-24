862907504cbc5e23d8d02dd2fdd6b4e78a4fc153
Piotr Kowalski <piecioshka@gmail.com>
2022-11-24 22:42:13 +0100

    Cleanup

--- src/main.js
+++ src/main.js
@@ -1,33 +1,40 @@
 function clearDOMElement($el) {
-    while ($el.firstElementChild) {
-        $el.firstElementChild.remove();
-    }
+  while ($el.firstElementChild) {
+    $el.firstElementChild.remove();
+  }
 }
+
+function compile(template) {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(template, "text/html");
+  return doc.body.firstElementChild;
+}
+
 function renderPage(template) {
-    const $main = document.querySelector('main');
-    const $page = new DOMParser().parseFromString(template, 'text/html').body.firstElementChild;
-    clearDOMElement($main)
-    $main.append($page);
+  const $main = document.querySelector("main");
+  const $page = compile(template);
+  clearDOMElement($main);
+  $main.append($page);
 }
 
 async function lazyLoadPage(pageId) {
-    const { page } = await import(`./pages/${pageId}.js`);
-    page();
+  const { page } = await import(`./pages/${pageId}.js`);
+  page();
 }
 
 function bindPageLink($button) {
-    $button.addEventListener('click', () => lazyLoadPage($button.dataset.page));
+  $button.addEventListener("click", () => lazyLoadPage($button.dataset.page));
 }
 
 function main() {
-    // Bind all buttons (should be links of course, this is only DEMO)
-    [
-        document.querySelector('button.home-btn'),
-        document.querySelector('button.contact-btn'),
-    ].forEach(bindPageLink);
+  // Bind all buttons (should be links of course, this is only DEMO)
+  [
+    document.querySelector("button.home-btn"),
+    document.querySelector("button.contact-btn"),
+  ].forEach(bindPageLink);
 
-    // Load default page
-    lazyLoadPage('home');
+  // Load default page
+  lazyLoadPage("home");
 }
 
 main();
